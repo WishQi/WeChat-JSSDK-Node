ff5aedbc4c97ac0273cb3a0226784882eb49953c
Li Maoqi <lmq729981607@gmail.com>
2016-12-13 12:03:45 +0800

    配置服务器

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,39 @@
 var router = require('koa-router')();
+var sign = require('../config/generateSign.js');
+var https = require('https');
+
+var appId = "wxf540ae16cc7b380d";
+var appSecret = "3adf5088350e3f063f81ff838e872d1b";
+var token = "WeChatMaoge";
 
 router.get('/', function *(next) {
-  yield this.render('wechat');
+
+    let access_token_promise = new Promise((resolve, reject) => {
+        https.get('https://sh.api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appSecret, (res) => {
+            resolve(res);
+        });
+    });
+
+    var access_token = yield access_token_promise.then((res) => {
+        return res.body['access_token'];
+    });
+
+    let jsapi_ticket_promise = new Promise((resolve, reject) => {
+        https.get('https://sh.api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + access_token + '&type=jsapi', (res) => {
+            resolve(res);
+        });
+    });
+
+    var jsapi_ticket = yield jsapi_ticket_promise.then((res) => {
+        return res.body['ticket'];
+    });
+
+    console.log('access_token:' + access_token);
+    console.log('jsapi_ticket:' + jsapi_ticket);
+
+    // var signature = sign(jsapi_ticket, 'http://www.use-mine.com/');
+    // console.log(signature);
+
 });
 
 module.exports = router;
