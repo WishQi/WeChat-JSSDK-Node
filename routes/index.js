8af45b5337f32958a4e621c34e62b1bcc1d5b8e4
Li Maoqi <lmq729981607@gmail.com>
2016-12-13 13:47:49 +0800

    配置服务器

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var router = require('koa-router')();
+var request = require('request');
 var sign = require('../config/generateSign.js');
 var https = require('https');
 
@@ -9,33 +10,40 @@ var token = "WeChatMaoge";
 router.get('/', function *(next) {
 
     let access_token_promise = new Promise((resolve, reject) => {
-        https.get('https://sh.api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appSecret, (res) => {
-            resolve(res);
+        request.get('https://sh.api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appSecret, (err, res, body) => {
+            resolve(body);
         });
     });
 
-    var access_token = yield access_token_promise.then((res) => {
-        console.log(res.body);
-        return res.body['access_token'];
-    });
-
-    let jsapi_ticket_promise = new Promise((resolve, reject) => {
-        https.get('https://sh.api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + access_token + '&type=jsapi', (res) => {
-            resolve(res);
-        });
-    });
-
-    var jsapi_ticket = yield jsapi_ticket_promise.then((res) => {
-        console.log(res.body);
-        return res.body['ticket'];
+    var access_token = yield access_token_promise.then((body) => {
+        var parseData = JSON.parse(body);
+        return parseData.access_token;
     });
 
     console.log('access_token:' + access_token);
+
+    let jsapi_ticket_promise = new Promise((resolve, reject) => {
+        request.get('https://sh.api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + access_token + '&type=jsapi', (err, res, body) => {
+            resolve(body);
+        });
+    });
+
+    var jsapi_ticket = yield jsapi_ticket_promise.then((body) => {
+        var parseData = JSON.parse(body);
+        return parseData.ticket;
+    });
+
     console.log('jsapi_ticket:' + jsapi_ticket);
 
-    // var signature = sign(jsapi_ticket, 'http://www.use-mine.com/');
-    // console.log(signature);
+    var signature = sign(jsapi_ticket, 'http://www.use-mine.com/');
+    console.log(signature);
 
+    yield this.render('wechat', {
+        appId: appId,
+        timestamp: signature.timestamp,
+        nonceStr: signature.nonceStr,
+        signature: signature.signature
+    });
 });
 
 module.exports = router;
