382cd67e5e585a075de08f9ca2332a8074c7a6f2
Li Maoqi <lmq729981607@gmail.com>
2016-12-14 18:07:11 +0800

    装jquery

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,6 @@ router.get('/', function *(next) {
         return parseData.access_token;
     });
 
-    console.log('access_token:' + access_token);
-
     let jsapi_ticket_promise = new Promise((resolve, reject) => {
         request.get('https://sh.api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + access_token + '&type=jsapi', (err, res, body) => {
             resolve(body);
@@ -32,8 +30,6 @@ router.get('/', function *(next) {
         var parseData = JSON.parse(body);
         return parseData.ticket;
     });
-
-    console.log('jsapi_ticket:' + jsapi_ticket);
 
     var signature = sign(jsapi_ticket, 'http://www.use-mine.com/');
     console.log(signature);
