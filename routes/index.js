9cb5f51e434ead6d47f9194d06bcd61719d7d518
Li Maoqi <lmq729981607@gmail.com>
2016-12-15 17:00:56 +0800

    接入arduino

--- routes/index.js
+++ routes/index.js
@@ -55,15 +55,21 @@ router.post('/handleRecord', function *(next) {
         deviceNo = 6;
     }
     console.log("deviceNo:", deviceNo);
+
     if (seconds <= 3) {
-        returnInfo[deviceNo] = 1;
+        returnInfo[deviceNo] += 1;
     } else if (seconds > 3 && seconds <= 6) {
-        returnInfo[deviceNo] = 2;
+        returnInfo[deviceNo] += 2;
     } else if ( seconds > 6 && seconds <= 9) {
-        returnInfo[deviceNo] = 3;
+        returnInfo[deviceNo] += 3;
     } else {
+        returnInfo[deviceNo] += 4;
+    }
+
+    if (returnInfo[deviceNo] > 4) {
         returnInfo[deviceNo] = 4;
     }
+
     console.log('returnInfo: ', returnInfo.join(''));
 });
 
